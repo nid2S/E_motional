ae76c722e7ebf8ba4454495a9f76651a77201f33
S20034N\user <s20034@gsm.hs.kr>
2021-07-12 17:26:16 +0900

    classifier - SpeechRecognition

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import load_model
-from textClassifier import preprocessing, make_model, SpeechRec
+from textClassifier import make_model, preprocessing, SpeechRec
 
 # make_model.imdb_modelMaking()
 
@@ -8,7 +8,7 @@ model = load_model("model/imdb.h5")
 test = preprocessing.imdb_preprocessing(SpeechRec.STT(5))
 
 prediction = model.predict(test)
-if prediction:    # 1 in prediction:
+if prediction:      # if prediction == 1
     print("긍정")
 else:
     print("부정")
