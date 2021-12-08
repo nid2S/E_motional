9516c72ef3686ebb35bdd2cdd8ab1fbf89e1ad1a
nid2S <s20034@gsm.hs.kr>
2021-12-08 23:08:37 +0900

    greed search

--- emotional/model_train.py
+++ emotional/model_train.py
@@ -13,20 +13,19 @@ p = Preprocesser(use_HF)
 def HF_model():
     return TFMobileBertForSequenceClassification.from_pretrained(p.MODEL_NAME, from_pt=True, num_labels=p.output_dim)
 
-def TF_model(use_rnn: bool = True, use_LSTM: bool = True) -> tf.keras.Model:
-    if use_rnn:
-        x = tf.keras.layers.Input(shape=(None, p.batch_size), batch_size=p.batch_size)
-        x = tf.keras.layers.Embedding(input_dim=p.input_dim, output_dim=p.embed_dim)(x)
-        if use_LSTM:
-            x = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.3)(x)
-        else:
-            x = tf.keras.layers.GRU(64, activation="tanh", dropout=0.3)(x)
-        x = tf.keras.layers.Dense(32, activation='relu', dropout=0.3)(x)
-
-        y = tf.keras.layers.Dense(p.output_dim, activation='softmax')(x)
-        return tf.keras.Model(x, y)
+def TF_model(use_LSTM: bool = True, use_Bidirectional: bool = False) -> tf.keras.Model:
+    x = tf.keras.layers.Input(shape=(None, p.batch_size), batch_size=p.batch_size)
+    x = tf.keras.layers.Embedding(input_dim=p.input_dim, output_dim=p.embed_dim)(x)
+    if use_LSTM:
+        x = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.3)(x)
     else:
-        pass
+        x = tf.keras.layers.GRU(64, activation="tanh", dropout=0.3)(x)
+    if use_Bidirectional:
+        x = tf.keras.layers.Bidirectional(x)
+    x = tf.keras.layers.Dense(32, activation='relu', dropout=0.3)(x)
+
+    y = tf.keras.layers.Dense(p.output_dim, activation='softmax')(x)
+    return tf.keras.Model(x, y)
 
 
 # hyper_param
@@ -68,11 +67,12 @@ else:
     history = ""
     for optim in ["adam", "rmsprop", "nadam"]:
         for order_RNN in ["LSTM", "GRU"]:
-            model = TF_model(use_LSTM=(order_RNN == "LSTM"))
-            model.compile(optim, loss, "accuracy")
-            hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
-                             callbacks=[EarlyStopping(monitor='val_loss', patience=3), LearningRateScheduler(lr_scheduler),
-                                        ModelCheckpoint("./model/emotion_"+order_RNN + "_" + optim, monitor="val_accuracy", save_best_only=True)])
-            model.save("./model/emotion_"+order_RNN + "_" + optim+".h5")
-            history += order_RNN + "_" + optim + ":" + str(hist) + "\n"
+            for use_Bi in ["Bi", ""]:
+                model = TF_model(use_LSTM=(order_RNN == "LSTM"), use_Bidirectional=(use_Bi=="Bi"))
+                model.compile(optim, loss, "accuracy")
+                hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
+                                 callbacks=[EarlyStopping(monitor='val_loss', patience=3), LearningRateScheduler(lr_scheduler),
+                                            ModelCheckpoint("./model/emotion_"+use_Bi+order_RNN+"_"+optim, monitor="val_accuracy", save_best_only=True)])
+                model.save("./model/emotion_"+order_RNN + "_" + optim+".h5")
+                history += use_Bi + order_RNN + "_" + optim + ":" + str(hist) + "\n"
     open("./model/history.txt", "w+", encoding="utf-8").write(history)
