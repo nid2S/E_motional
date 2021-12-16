7a4d8d73f64284770a00ac382472106c8591cd38
nid2S <s20034@gsm.hs.kr>
2021-12-16 19:48:10 +0900

    some

--- emotional/model_train.py
+++ emotional/model_train.py
@@ -19,9 +19,9 @@ def TF_model(use_LSTM: bool = True, use_Bidirectional: bool = False) -> tf.keras
     x = tf.keras.layers.Embedding(input_dim=p.vocab_size, output_dim=p.embed_dim)(input_layer)
 
     if use_LSTM:
-        RNN_layer = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.3)
+        RNN_layer = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.1)
     else:
-        RNN_layer = tf.keras.layers.GRU(64, activation="tanh", dropout=0.3)
+        RNN_layer = tf.keras.layers.GRU(64, activation="tanh", dropout=0.1)
     if use_Bidirectional:
         RNN_layer = tf.keras.layers.Bidirectional(RNN_layer)
 
@@ -53,7 +53,9 @@ def lr_scheduler(epoch, lr):
         else:
             return 1e-5
     else:
-        if epoch < 10:
+        if epoch < 5:
+            return 0.1
+        elif epoch < 10:
             return 0.01
         else:
             return 0.001 * tf.exp(-0.1)
@@ -78,18 +80,18 @@ else:
                 model = TF_model(use_LSTM=(order_RNN == "LSTM"), use_Bidirectional=(use_Bi == "Bi"))
                 model.compile(optim, loss, "accuracy")
                 hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
-                                 callbacks=[EarlyStopping(monitor='val_loss', patience=3), LearningRateScheduler(lr_scheduler),
+                                 callbacks=[EarlyStopping(monitor='val_loss', mode="min", patience=5), LearningRateScheduler(lr_scheduler),
                                             ModelCheckpoint("./model/emotion_"+use_Bi+order_RNN+"_"+optim, monitor="val_accuracy", save_best_only=True)])
 
-                plt.plot(range(1, epochs+1), hist.history["loss"], "r", label="loss")
-                plt.plot(range(1, epochs+1), hist.history["accuracy"], "b", label="accuracy")
-                plt.plot(range(1, epochs+1), hist.history["val_loss"], "g", label="val_loss")
-                plt.plot(range(1, epochs+1), hist.history["val_accuracy"], "k", label="val_accuracy")
+                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["loss"], "r", label="loss")
+                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["accuracy"], "b", label="accuracy")
+                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_loss"], "g", label="val_loss")
+                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_accuracy"], "k", label="val_accuracy")
                 plt.title(optim+"_"+use_Bi+order_RNN)
                 plt.text(5, 3, str(max(hist.history["val_accuracy"])))
                 plt.xlabel("epoch")
                 plt.ylabel("loss/accuracy")
-                plt.xticks(range(1, epochs+1))
-                plt.xlim(0.9, epochs+0.1)
+                plt.xticks(range(1, len(hist.history["loss"])+1))
+                plt.xlim(0.9, len(hist.history["loss"])+0.1)
                 plt.legend()
     plt.savefig("./history.png")
