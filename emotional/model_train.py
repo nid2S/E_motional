b19134abe1225bac499ebaebff42f342ff013e28
nid2S <s20034@gsm.hs.kr>
2021-12-22 16:16:51 +0900

    structure change

--- emotional/model_train.py
+++ emotional/model_train.py
@@ -14,19 +14,30 @@ p = Preprocesser(use_HF)
 def HF_model():
     return TFMobileBertForSequenceClassification.from_pretrained(p.MODEL_NAME, from_pt=True, num_labels=p.output_dim)
 
-def TF_model(use_LSTM: bool = True, use_Bidirectional: bool = False) -> tf.keras.Model:
+def TF_model(order_model: str = "LSTM", use_Bidirectional: bool = False) -> tf.keras.Model:
+    assert order_model in ["LSTM", "CNN", "attention"]
+
     input_layer = tf.keras.layers.Input(shape=p.input_dim)
     x = tf.keras.layers.Embedding(input_dim=p.vocab_size, output_dim=p.embed_dim)(input_layer)
+    # x = tf.keras.layers.LayerNormalization()(x)
 
-    if use_LSTM:
-        RNN_layer = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.1)
+    if order_model == "RNN":
+        model_layer = tf.keras.layers.LSTM(64, activation="tanh", dropout=0.3)
+        if use_Bidirectional:
+            model_layer = tf.keras.layers.Bidirectional(model_layer)
+    elif order_model == "CNN":
+        x = tf.keras.layers.Conv1D(32, 4, activation="relu")(x)
+        x = tf.keras.layers.MaxPooling1D(pool_size=2, strides=1)(x)
+        x = tf.keras.layers.Dropout(0.3)(x)
+
+        x = tf.keras.layers.Conv1D(32, 3, activation="relu")(x)
+        x = tf.keras.layers.MaxPooling1D(pool_size=2, strides=1)(x)
+        model_layer = tf.keras.layers.Dropout(0.3)
     else:
-        RNN_layer = tf.keras.layers.GRU(64, activation="tanh", dropout=0.1)
-    if use_Bidirectional:
-        RNN_layer = tf.keras.layers.Bidirectional(RNN_layer)
+        model_layer = None
 
-    x = RNN_layer(x)
-    x = tf.keras.layers.Dense(32, activation='relu')(x)
+    x = model_layer(x)
+    x = tf.keras.layers.Dense(16, activation='relu')(x)
 
     y = tf.keras.layers.Dense(p.output_dim, activation='softmax')(x)
     return tf.keras.Model(input_layer, y)
@@ -68,33 +79,36 @@ if use_HF:
     hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
                      callbacks=[EarlyStopping(monitor='val_loss', patience=3), LearningRateScheduler(lr_scheduler),
                                 ModelCheckpoint("./model/emotion_classification", monitor="val_accuracy", save_best_only=True)])
-    model.save("./model/emotion.h5")
 else:
     pos = 1
-    for optim in ["adam", "rmsprop", "nadam"]:
-        for order_RNN in ["LSTM", "GRU"]:
-            for use_Bi in ["", "Bi"]:
-                plt.subplot(3, 4, pos)
-                plt.figure(figsize=(50, 50))
-                pos += 1
+    for model_order in ["LSTM", "CNN", "attention"]:
+        for use_Bi in ["", "Bi"]:
+            if model_order != "LSTM" and use_Bi == "Bi":
+                continue
+            if model_order == "attention":
+                continue
 
-                model = TF_model(use_LSTM=(order_RNN == "LSTM"), use_Bidirectional=(use_Bi == "Bi"))
-                model.compile(optim, loss, "accuracy")
-                hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
-                                 callbacks=[EarlyStopping(monitor='val_loss', mode="min", patience=5), LearningRateScheduler(lr_scheduler),
-                                            ModelCheckpoint("./model/"+use_Bi+order_RNN+"/"+optim+"_max_accuracy", monitor="val_accuracy", save_best_only=True),
-                                            ModelCheckpoint("./model/"+use_Bi+order_RNN+"/"+optim+"_min_loss", mode="min", monitor="val_loss", save_best_only=True)])
+            plt.subplot(3, 4, pos)
+            plt.figure(figsize=(50, 50))
+            pos += 1
 
-                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["loss"], "r", label="loss")
-                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["accuracy"], "b", label="accuracy")
-                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_loss"], "g", label="val_loss")
-                plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_accuracy"], "k", label="val_accuracy")
-                plt.title(optim+"_"+use_Bi+order_RNN)
-                plt.text(1, 3, "acc : %.1f" % str(max(hist.history["val_accuracy"])))
-                plt.text(1, 6, "loss : %.1f" % str(min(hist.history["val_loss"])))
-                plt.xlabel("epoch")
-                plt.ylabel("loss/accuracy")
-                plt.xticks(range(1, len(hist.history["loss"])+1))
-                plt.xlim(0.9, len(hist.history["loss"])+0.1)
-                plt.legend()
-    plt.savefig("./history.png")
+            model = TF_model(order_model=model_order, use_Bidirectional=(use_Bi == "Bi"))
+            model.compile("adam", loss, "accuracy")
+            hist = model.fit(p.getTrainDataset(), validation_data=p.getValidationDataset(), batch_size=p.batch_size, epochs=epochs,
+                             callbacks=[EarlyStopping(monitor='val_loss', mode="min", patience=7), LearningRateScheduler(lr_scheduler),
+                                        ModelCheckpoint("./model/"+use_Bi+model_order+"/max_accuracy", monitor="val_accuracy", save_best_only=True),
+                                        ModelCheckpoint("./model/"+use_Bi+model_order+"/min_loss", mode="min", monitor="val_loss", save_best_only=True)])
+
+            plt.plot(range(1, len(hist.history["loss"])+1), hist.history["loss"], "r", label="loss")
+            plt.plot(range(1, len(hist.history["loss"])+1), hist.history["accuracy"], "b", label="accuracy")
+            plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_loss"], "g", label="val_loss")
+            plt.plot(range(1, len(hist.history["loss"])+1), hist.history["val_accuracy"], "k", label="val_accuracy")
+            plt.title(use_Bi+model_order)
+            plt.text(1, 3, "max val_acc : %.1f" % max(hist.history["val_accuracy"]))
+            plt.text(1, 6, "min val_loss : %.1f" % min(hist.history["val_loss"]))
+            plt.xlabel("epoch")
+            plt.ylabel("loss/accuracy")
+            plt.xticks(range(1, len(hist.history["loss"])+1))
+            plt.xlim(0.9, len(hist.history["loss"])+0.1)
+        plt.legend()
+        plt.savefig("./history.png")
