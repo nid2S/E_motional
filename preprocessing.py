5d17dfb46371cfc12c9e374849ec335a49c372ba
nid2S <s20034@gsm.hs.kr>
2022-02-06 20:28:28 +0900

    change labels

---
+++ preprocessing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def change_dataset():
+    train = pd.read_csv("./data/train.txt", sep="\t", encoding="utf-8")
+    val = pd.read_csv("./data/val.txt", sep="\t", encoding="utf-8")
+    label_dict = {0: [0], 1: [1], 2: [2], 3: [3, 4, 8], 4: [5, 6], 5: [7, 9], 6: [10]}
+    for new_label, past_labels in label_dict.items():
+        train["label"] = train["label"].apply(lambda x: new_label if x in past_labels else x)
+        val["label"] = val["label"].apply(lambda x: new_label if x in past_labels else x)
+    train.to_csv("./data/train.txt", sep="\t", encoding="utf-8")
+    val.to_csv("./data/val.txt", sep="\t", encoding="utf-8")
