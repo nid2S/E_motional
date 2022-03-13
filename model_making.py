e0425c5949486be4ce687fbadeb8699974c4e7a0
nid2S <s20034@gsm.hs.kr>
2022-03-13 21:29:14 +0900

    E_motional model base code

--- model_making.py
+++ model_making.py
@@ -1,177 +1,160 @@
 import setuptools
-from torch.utils.data import TensorDataset, DataLoader
+import re
 import torch
-from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-import pytorch_lightning as pl
-from konlpy.tag import Hannanum
-from transformers import BertForSequenceClassification, BertTokenizerFast
+import math
 import pandas as pd
 import argparse
-import re
+import pytorch_lightning as pl
+from torch.optim import AdamW
+from torch.utils.data import TensorDataset, DataLoader
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
+from transformers.optimization import get_cosine_schedule_with_warmup
+from transformers import GPT2TokenizerFast
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-hf', type=bool, default=False, dest="use_hf", help='condition of using HuggingFace Model')
-args = parser.parse_args()
+parser.add_argument("-e", type=int, default=50, dest="epochs", help="num of epochs")
+parser.add_argument("-b", type=int, default=32, dest="batch_size", help="size of each batch")
+parser.add_argument("-h", type=int, default=256, dest="hidden_size", help="size of hidden_state")
+parser.add_argument("-l", type=int, default=12, dest="num_layers", help="num of transfomer model encoder layers")
+parser.add_argument("-p", type=int, default=5, dest="patience", help="number of check with no improved")
+parser.add_argument("-lr", type=float, default=0.01, dest="learning_rate", help="learning rate")
+parser.add_argument("-dr", type=float, default=0.1, dest="dropout_rate", help="dropout rate")
+parser.add_argument("-wr", type=float, default=0.05, dest="warmup_ratio", help="warmup rate")
+parser.add_argument("--embedding-size", type=int, default=512, dest="embedding_size", help="size of embedding vector")
 
-class EmotionClassification(LightningModule):
-    def __init__(self):
-        super(EmotionClassification, self).__init__()
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, model_dim, input_dim, dropout_rate):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = torch.nn.Dropout(dropout_rate)
+        pos_encoding = torch.zeros(input_dim, model_dim)
+        position_list = torch.arange(input_dim, dtype=torch.float).view(-1, 1)  # unsqueez(1)
+        division_term = torch.exp(torch.arange(0, model_dim, 2).float()*(-math.log(10000)/model_dim))
+        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
+        pos_encoding[:, 0::2] = torch.sin(position_list * division_term)
+        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
+        pos_encoding[:, 1::2] = torch.cos(position_list * division_term)
+        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pos_encoding", pos_encoding)
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
+        return self.dropout(x + self.pos_encoding[:x.size(0)])
+
+class EmotionClassifier(LightningModule):
+    def __init__(self, hparams):
+        super(EmotionClassifier, self).__init__()
         self.RANDOM_SEED = 7777
-        torch.manual_seed(self.RANDOM_SEED)
-        torch.cuda.manual_seed(self.RANDOM_SEED)
         pl.seed_everything(self.RANDOM_SEED)
 
-        self.USE_HF = args.use_hf
+        self.epochs = hparams.epochs
+        self.batch_size = hparams.batch_size
+        self.embedding_size = hparams.embedding_size
+        self.hidden_size = hparams.hidden_size
+        self.num_layers = hparams.num_layers
+        self.patience = hparams.patience
+        self.learning_rate = hparams.learning_rate
+        self.dropout_rate = hparams.dropout_rate
+        self.warmup_ratio = hparams.warmup_ratio
+
+        self.num_labels = 7
+        self.input_dim = None
+
+        self.label_dict = None
         self.train_set = None
         self.val_set = None
-        self.test_set = None
+        self.tokenizer = None
+        self.pad_token_id = self.tokenizer.pad_token_id
 
-        self.learning_rate = 0.1
-        self.num_layers = 2
-        self.num_labels = 7
-        self.batch_size = 32
-        self.input_dim = 55
-        self.embedding_size = 128
-        self.hidden_size = 64
-
-        self.tokenizer = Hannanum()
-        self.vocab = dict([(token, index) for _, (token, index) in pd.read_csv("./data/vocab.txt", sep="\t", index_col=0, encoding="utf-8").iterrows()])
-        self.id_to_token = dict([(index, token) for token, index in self.vocab.items()])
-        self.vocab_size = len(self.vocab) + 1
-        self.pad_token_id = self.vocab['<pad>']  # 0
-
-        self.embedding = torch.nn.Embedding(self.input_dim, self.embedding_size, padding_idx=self.pad_token_id)
-        self.LSTM = torch.nn.LSTM(self.embedding_size, self.hidden_size, num_layers=self.num_layers, batch_first=True, dropout=0.3)
-        self.fc = torch.nn.Linear(self.hidden_size, self.num_labels)
-        self.model = None
-
-        if self.USE_HF:
-            self.model = BertForSequenceClassification.from_pretrained("Huffon/klue-roberta-base-nli", num_labels=self.num_labels)
-            self.tokenizer = BertTokenizerFast.from_pretrained("Huffon/klue-roberta-base-nli")
-            self.pad_token_id = self.tokenizer.pad_token_id
-            self.learning_rate = 3e-5
-            self.input_dim = 125
+        self.embedding_layer = torch.nn.Sequential(
+            torch.nn.Embedding(self.tokenizer.vocab_size, self.embedding_size, self.pad_token_id),
+            torch.nn.Linear(self.embedding_size, self.embedding_size),
+            torch.nn.ELU(),
+            torch.nn.LayerNorm(self.embedding_size, eps=1e-5, elementwise_affine=True)
+        )
+        transformerEncoder_layer = torch.nn.TransformerEncoderLayer(self.embedding_size, 8, dropout=self.dropout_rate, batch_first=True)
+        self.transformerEncoder = torch.nn.Sequential(
+            PositionalEncoding(self.embedding_size, self.input_dim, self.dropout_rate),
+            torch.nn.TransformerEncoder(transformerEncoder_layer, self.num_layers, norm=torch.nn.LayerNorm(self.embedding_size, eps=1e-5, elementwise_affine=True))
+        )
+        self.model_output_layer = torch.nn.Sequential(
+            torch.nn.Linear(self.embedding_size, self.hidden_size),
+            torch.nn.ELU(),
+            torch.nn.LayerNorm(self.hidden_size, eps=1e-5, elementwise_affine=True),
+            torch.nn.Dropout(self.dropout_rate)
+        )
+        self.output_layer = torch.nn.Sequential(
+            torch.nn.Linear(self.input_dim*self.hidden_size, self.num_labels),
+            torch.nn.Softmax(dim=1)
+        )
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+        optim = AdamW(self.parameters(), lr=self.learning_rate, weight_decay=0.1)
+
+        num_train_steps = len(self.train_dataloader()) * self.epochs
+        num_warmup_steps = int(num_train_steps * self.warmup_ratio)
+        scheduler = get_cosine_schedule_with_warmup(optim, num_warmup_steps=num_warmup_steps, num_training_steps=num_train_steps)
+        lr_scheduler = {'scheduler': scheduler, 'name': 'cosine_schedule_with_warmup', 'monitor': 'loss', 'interval': 'step', 'frequency': 1}
+        return [optim], [lr_scheduler]
+
+    def configure_callbacks(self):
+        model_checkpoint = ModelCheckpoint(dirpath=f"./model/model_ckp/", filename='{epoch:02d}_{loss:.2f}', verbose=True, save_last=True, monitor='val_loss', mode='min')
+        early_stopping = EarlyStopping(monitor="val_loss", mode="min", patience=self.patience)
+        lr_monitor = LearningRateMonitor(logging_interval="step")
+        return [model_checkpoint, early_stopping, lr_monitor]
 
     def forward(self, x):
-        if self.USE_HF:
-            output = self.model(x)
-            y = output.logits
-        else:
-            x = self.embedding(x)
-            h_0 = torch.zeros(self.num_layers, self.batch_size, self.hidden_size, requires_grad=True)
-            c_0 = torch.zeros(self.num_layers, self.batch_size, self.hidden_size, requires_grad=True)
-            x, (h_n, c_n) = self.LSTM(x, (h_0, c_0))
-            x = x.view(-1, self.hidden_size)
-            y = self.fc(x)
-        return y
+        x = self.embedding_layer(x)
+        x = self.transformerEncoder(x)
+        x = self.model_output_layer(x)
+        output = self.output_layer(x.view(self.batch_size, self.input_dim*self.hidden_size))
+        return output
 
-    def cross_entropy_loss(self, output, labels):
-        return torch.nn.CrossEntropyLoss()(output, labels, ignore_index=self.pad_token_id)
+    def loss(self, output, labels):
+        return torch.nn.CrossEntropyLoss()(output, labels)
 
     def accuracy(self, output, labels):
-        self.parameters()
         output = torch.argmax(output, dim=1)
         return torch.sum(output == labels) / output.__len__() * 100  # %(Precentage)
 
     def prepare_data(self):
-        raw_train = pd.read_csv("./data/train.txt", sep="\t", encoding="utf-8", index_col=0)
-        raw_val = pd.read_csv("./data/val.txt", sep="\t", encoding="utf-8", index_col=0)
-        raw_test = pd.read_csv("./data/test.txt", sep="\t", encoding="utf-8", index_col=0)
-
-        train_Y = torch.LongTensor(raw_train["label"].to_list())
-        val_Y = torch.LongTensor(raw_val["label"].to_list())
-        test_Y = torch.LongTensor(raw_test["label"].to_list())
-
-        if self.USE_HF:
-            train_x = self.tokenizer.batch_encode_plus(raw_train["data"].to_list(), return_tensors="pt",
-                                                       max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
-            val_x = self.tokenizer.batch_encode_plus(raw_val["data"].to_list(), return_tensors="pt",
-                                                     max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
-            test_x = self.tokenizer.batch_encode_plus(raw_test["data"].to_list(), return_tensors="pt",
-                                                      max_length=self.max_len, padding="max_length", truncation=True)["input_ids"]
-        else:
-            train_x = torch.FloatTensor(raw_train["data"].apply(lambda x: self.tokenize(x, return_tensor=False)).to_list())
-            val_x = torch.FloatTensor(raw_val["data"].apply(lambda x: self.tokenize(x, return_tensor=False)).to_list())
-            test_x = torch.FloatTensor(raw_test["data"].apply(lambda x: self.tokenize(x, return_tensor=False)).to_list())
-
-        self.train_set = TensorDataset(train_x, train_Y)
-        self.val_set = TensorDataset(val_x, val_Y)
-        self.test_set = TensorDataset(test_x, test_Y)
+        pass  # TODO
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True, drop_last=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_set, batch_size=self.batch_size, shuffle=True)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.val_set, batch_size=self.batch_size, shuffle=False, drop_last=True)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_pred = self(x)
-        loss = self.cross_entropy_loss(y_pred, y)
+        loss = self.loss(y_pred, y)
         accuracy = self.accuracy(y_pred, y)
 
-        logs = {'train_loss': loss, 'train_acc': accuracy}
-        return {'loss': loss, 'accuracy': accuracy, 'log': logs}
+        self.log('loss', loss)
+        self.log('acc', accuracy, prog_bar=True)
+        return {'loss': loss, 'acc': accuracy}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_pred = self(x)
-        loss = self.cross_entropy_loss(y_pred, y)
+        loss = self.loss(y_pred, y)
         accuracy = self.accuracy(y_pred, y)
 
-        logs = {'val_loss': loss, 'val_acc': accuracy}
-        return {'loss': loss, 'accuracy': accuracy, 'log': logs}
+        self.log_dict({'val_loss': loss, 'val_acc': accuracy}, prog_bar=True)
+        return {'val_loss': loss, 'val_acc': accuracy}
 
     def validation_epoch_end(self, outputs):
         mean_loss = torch.stack([output['val_loss'] for output in outputs]).mean()
         mean_acc = torch.stack([output['val_acc'] for output in outputs]).mean()
-        logs = {'val_loss': mean_loss, 'val_acc': mean_acc}
-        return {'avg_val_loss': mean_loss, 'avg_val_acc': mean_acc, 'log': logs}
 
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        y_pred = self(x)
-        loss = self.cross_entropy_loss(y_pred, y)
-        accuracy = self.accuracy(y_pred, y)
-
-        logs = {'test_loss': loss, 'test_acc': accuracy}
-        return {'loss': loss, 'accuracy': accuracy, 'log': logs}
-
-    def test_epoch_end(self, outputs):
-        mean_loss = torch.stack([output['test_loss'] for output in outputs]).mean()
-        mean_acc = torch.stack([output['test_acc'] for output in outputs]).mean()
-        logs = {'test_loss': mean_loss, 'test_acc': mean_acc}
-        return {'avg_test_loss': mean_loss, 'avg_test_acc': mean_acc, 'log': logs}
-
-    def tokenize(self, text, return_tensor: bool = True):
-        if self.USE_HF:
-            self.tokenizer.encode(text, max_length=self.input_dim, padding="max_length", truncation=True, return_tensors="pt")
-        else:
-            # N - 체언 | P - 용언 | F - 외국어
-            text = re.sub(r"\W", r" ", text).strip()
-            text = [token for (token, tag) in self.tokenizer.pos(text) if ('N' in tag) or ('P' in tag) or ('F' in tag)]
-            for i, token in enumerate(text):
-                text[i] = self.vocab[token] if token in self.vocab else self.vocab['<oov>']
-            text = text + [self.pad_token_id] * (self.input_dim - len(text))
-            text = text[:self.input_dim]
-            return torch.FloatTensor(text) if return_tensor else text
+        self.log_dict({'avg_val_loss': mean_loss, 'avg_val_acc': mean_acc}, on_epoch=True, prog_bar=True)
+        return {'avg_val_loss': mean_loss, 'avg_val_acc': mean_acc}
 
 
-epochs = 4 if args.use_hf else 100
-dir_name = "hf_model" if args.use_hf else "pl_model"
-model = EmotionClassification()
-trainer = Trainer(max_epochs=epochs, gpus=torch.cuda.device_count(),
-                  callbacks=[ModelCheckpoint("./model/"+dir_name+"/check_point/", verbose=True, save_top_k=3, monitor="val_acc", mode="max"),
-                             EarlyStopping(monitor="val_loss", mode="min", patience=3)])
-
+args = parser.parse_args()
+model = LabelClassifier(args)
+trainer = Trainer(max_epochs=args.epochs, gpus=torch.cuda.device_count(), logger=TensorBoardLogger("./model/tensorboardLog/"))
 trainer.fit(model)
-torch.save(model.state_dict(), "./model/" + dir_name + "/torch_model.pt")
-trainer.save_checkpoint("./model/" + dir_name + "/pl_model.ptl")
+torch.save(model.state_dict(), f"./model/model_state.pt")
